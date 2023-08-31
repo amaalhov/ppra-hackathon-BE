c66969b43be650529b9bba61fcd480dcf0bca5cd
amaahov <amleshiba@gmail.com>
2023-08-31 19:29:30 +0200

    updated auth routes

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import { currentUser } from "@le-ma/common";
+import express from 'express';
+import { currentUser } from '@le-ma/common';
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, (req, res) => {
+router.get('/api/auth/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
