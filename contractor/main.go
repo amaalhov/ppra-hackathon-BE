7c334c411d19d458a407190042ac14b026240cbb
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-03 14:29:23 +0200

    ci: trigger github actions

--- contractor/main.go
+++ contractor/main.go
@@ -15,6 +15,7 @@ import (
 )
 
 func main() {
+	// wait to connect to postgres db server
 	time.Sleep(30 * time.Second)
 	dbUrl := "postgresql://admin:psltest@postgresdb:5432/postgres?sslmode=disable"
 	pool, err := pgxpool.New(context.Background(), dbUrl)
